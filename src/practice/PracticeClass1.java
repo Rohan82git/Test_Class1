4f24a51655fd89e2055641295b2327e658668463
RohanKhochare <RDKhochare@outlook.com>
2024-12-29 12:57:27 +0530

    Modified1

--- src/practice/PracticeClass1.java
+++ src/practice/PracticeClass1.java
@@ -3,8 +3,17 @@ package practice;
 public class PracticeClass1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PracticeClass1 p = new PracticeClass1();
+		int a =p.main("aa1 kan 33k s4h 5a7 89");
+		System.out.println(a);
 	}
-
+	int main(String str) {
+		int count=0;
+		for(int i=0;i<str.length();i++) {
+			char ch = str.charAt(i);
+			if(ch == ' ') {
+				count++;
+			}
+		}return count;
+	}
 }
